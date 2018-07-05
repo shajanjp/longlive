daed007338aa3dc32ff17a3323327a0a53d5766e
Shajan Jacob <shajanjp@gmail.com>
2018-07-05 08:52:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+var robot = require('robotjs');
+const argv = require('yargs').argv;
+
+function makeMinutes (hours, minutes) {
+  let totalMinutes = minutes;
+  totalMinutes += hours * 60;
+  return totalMinutes;
+}
+
+if (argv.deploy) {
+  console.log('loaded...');
+  let curTime;
+  let tym;
+  let inMinutes;
+  robot.setMouseDelay(2);
+
+  function doThisThing () {
+  let mouse = robot.getMousePos();
+    curTime = new Date();
+    inMinutes = makeMinutes(curTime.getHours(), curTime.getMinutes());
+    if ((inMinutes > 510 && inMinutes < 790) || (inMinutes > 830 && inMinutes < 1085)) {
+      robot.moveMouse(mouse.x + 1, mouse.y + 1);
+      // console.log('Running...');
+    } else {
+      console.log('false');
+    }
+    setTimeout(doThisThing, 60000);
+  }
+
+  doThisThing();
+}
